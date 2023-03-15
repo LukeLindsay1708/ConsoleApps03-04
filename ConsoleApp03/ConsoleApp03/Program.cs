e35c55511f4e869cb90c55bb1020f3874abf6dfa
LukeLindsay1708 <115123472+LukeLindsay1708@users.noreply.github.com>
2023-03-15 12:09:10 +0000

    Base lines of the code

---
+++ ConsoleApp03/ConsoleApp03/Program.cs
@@ -0,0 +1,261 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("---------------------------------------------------------");
+        Console.WriteLine("------------ Luke's Student Marks Calculator ------------");
+        Console.WriteLine("-----------------  Student ID 22206266 ------------------");
+        Console.WriteLine("-------------- Buckinghamhshire University --------------");
+        Console.WriteLine("---------------------------------------------------------");
+
+        Console.WriteLine("Lets enter all the students names and marks ");
+        Console.WriteLine("Enter students name: ");
+        string Student1 = Convert.ToString(Console.ReadLine());
+
+        Console.WriteLine("How many marks did " + Student1 + " get?: ");
+        int MStudent1 = Convert.ToInt32(Console.ReadLine());
+        
+        string grade;
+        if (MStudent1 >= 70)
+        { grade = " First ";}
+
+        else if (MStudent1 >= 50)
+        { grade = " Upper Second Class "; }
+
+        else if (MStudent1 >= 40)
+        { grade = " A lower Second Class "; }
+
+        else { grade = " Fail "; }
+        string Total1 = ( Student1 + " got " + MStudent1 + " marks, which is a" + grade);
+        
+        Console.WriteLine("--------------------------------------");
+
+        Console.WriteLine("Enter a new students name: ");
+        string Student2 = Convert.ToString(Console.ReadLine());
+
+        Console.WriteLine("How many marks did " + Student2 + " get?: ");
+        int MStudent2 = Convert.ToInt32(Console.ReadLine());
+
+        string grade2;
+        if (MStudent2 >= 70)
+        { grade2 = " First "; }
+
+        else if (MStudent2 >= 50)
+        { grade2 = " Upper Second Class "; }
+
+        else if (MStudent2 >= 40)
+        { grade2 = " A lower Second Class "; }
+
+        else { grade2 = " Fail "; }
+
+        string Total2 = (Student2 + " got " + MStudent2 + " marks, which is a" + grade2);
+
+        Console.WriteLine("--------------------------------------");
+
+        Console.WriteLine("Enter a new students name: ");
+        string Student3 = Convert.ToString(Console.ReadLine());
+
+        Console.WriteLine("How many marks did " + Student3 + " get?: ");
+        int MStudent3 = Convert.ToInt32(Console.ReadLine());
+
+        string grade3;
+        if (MStudent3 >= 70)
+        { grade3 = " First "; }
+
+        else if (MStudent3 >= 50)
+        { grade3 = " Upper Second Class "; }
+
+        else if (MStudent3 >= 40)
+        { grade3 = " A lower Second Class "; }
+
+        else { grade3 = " Fail "; }
+
+        string Total3 = (Student3 + " got " + MStudent3 + " marks, which is a" + grade3);
+
+        Console.WriteLine("--------------------------------------");
+
+        Console.WriteLine("Enter a new students name: ");
+        string Student4 = Convert.ToString(Console.ReadLine());
+
+        Console.WriteLine("How many marks did " + Student4 + " get?: ");
+        int MStudent4 = Convert.ToInt32(Console.ReadLine());
+
+        string grade4;
+        if (MStudent2 >= 70)
+        { grade4 = " First "; }
+
+        else if (MStudent4 >= 50)
+        { grade4 = " Upper Second Class "; }
+
+        else if (MStudent4 >= 40)
+        { grade4 = " A lower Second Class "; }
+
+        else { grade4 = " Fail "; }
+
+        string Total4 = (Student4 + " got " + MStudent4 + " marks, which is a" + grade4);
+
+        Console.WriteLine("--------------------------------------");
+
+        Console.WriteLine("Enter a new students name: ");
+        string Student5 = Convert.ToString(Console.ReadLine());
+
+        Console.WriteLine("How many marks did " + Student5 + " get?: ");
+        int MStudent5 = Convert.ToInt32(Console.ReadLine());
+
+        string grade5;
+        if (MStudent5 >= 70)
+        { grade5 = " First "; }
+
+        else if (MStudent5 >= 50)
+        { grade5 = " Upper Second Class "; }
+
+        else if (MStudent5 >= 40)
+        { grade5 = " A lower Second Class "; }
+
+        else { grade5 = " Fail "; }
+
+        string Total5 = (Student5 + " got " + MStudent5 + " marks, which is a" + grade5);
+
+        Console.WriteLine("--------------------------------------");
+
+        Console.WriteLine("Enter a new students name: ");
+        string Student6 = Convert.ToString(Console.ReadLine());
+
+        Console.WriteLine("How many marks did " + Student6 + " get?: ");
+        int MStudent6 = Convert.ToInt32(Console.ReadLine());
+
+        string grade6;
+        if (MStudent6 >= 70)
+        { grade6 = " First "; }
+
+        else if (MStudent6 >= 50)
+        { grade6 = " Upper Second Class "; }
+
+        else if (MStudent6 >= 40)
+        { grade6 = " A lower Second Class "; }
+
+        else { grade6 = " Fail "; }
+
+        string Total6 = (Student6 + " got " + MStudent6 + " marks, which is a" + grade6);
+
+        Console.WriteLine("--------------------------------------");
+
+        Console.WriteLine("Enter a new student's name: ");
+        string Student7 = Convert.ToString(Console.ReadLine());
+
+        Console.WriteLine("How many marks did " + Student7 + " get?: ");
+        int MStudent7 = Convert.ToInt32(Console.ReadLine());
+
+        string grade7;
+        if (MStudent7 >= 70)
+        {grade7 = " First ";}
+
+        else if (MStudent7 >= 50)
+        {grade7 = " Upper Second Class ";}
+
+        else if (MStudent7 >= 40)
+        {grade7 = " A lower Second Class ";}
+
+        else
+        {grade7 = " Fail ";}
+
+        string Total7 = (Student7 + " got " + MStudent7 + " marks, which is a" + grade7);
+
+        Console.WriteLine("--------------------------------------");
+
+        Console.WriteLine("Enter a new student's name: ");
+        string Student8 = Convert.ToString(Console.ReadLine());
+
+        Console.WriteLine("How many marks did " + Student8 + " get?: ");
+        int MStudent8 = Convert.ToInt32(Console.ReadLine());
+
+        string grade8;
+        if (MStudent8 >= 70)
+        {grade8 = " First ";}
+
+        else if (MStudent8 >= 50)
+        {grade8 = " Upper Second Class ";}
+
+        else if (MStudent8 >= 40)
+        { grade8 = " A lower Second Class ";}
+
+        else
+        {grade8 = " Fail ";}
+
+        string Total8 = (Student8 + " got " + MStudent8 + " marks, which is a" + grade8);
+
+        Console.WriteLine("--------------------------------------");
+
+        Console.WriteLine("Enter a new student's name: ");
+        string Student9 = Convert.ToString(Console.ReadLine());
+
+        Console.WriteLine("How many marks did " + Student9 + " get?: ");
+        int MStudent9 = Convert.ToInt32(Console.ReadLine());
+
+        string grade9;
+        if (MStudent9 >= 70)
+        { grade9 = " First "; }
+
+        else if (MStudent9 >= 50)
+        { grade9 = " Upper Second Class "; }
+
+        else if (MStudent9 >= 40)
+        { grade9 = " A lower Second Class "; }
+
+        else { grade9 = " Fail "; }
+
+        string Total9 = (Student9 + " got " + MStudent9 + " marks, which is a" + grade9);
+
+        Console.WriteLine("--------------------------------------");
+
+        Console.WriteLine("Enter a new students name: ");
+        string Student10 = Convert.ToString(Console.ReadLine());
+
+        Console.WriteLine("How many marks did " + Student10 + " get?: ");
+        int MStudent10 = Convert.ToInt32(Console.ReadLine());
+
+        string grade10;
+        if (MStudent10 >= 70)
+        { grade10 = " First"; }
+
+        else if (MStudent10 >= 50)
+        { grade10 = " First Class"; }
+
+        else if (MStudent10 >= 40)
+        { grade10 = " Lower Second Class"; }
+
+        else { grade10 = " Fail"; }
+
+        string Total10 = (Student10 + " got " + MStudent10 + " marks, which is a" + grade10);
+
+        Console.WriteLine(Total10);
+
+        Console.WriteLine("------------------------");
+
+        Console.WriteLine("Here are everyones marks");
+
+        Console.WriteLine(Total1);
+        Console.WriteLine(Total2);
+        Console.WriteLine(Total3);
+        Console.WriteLine(Total4);
+        Console.WriteLine(Total5);
+        Console.WriteLine(Total6);
+        Console.WriteLine(Total7);
+        Console.WriteLine(Total8);
+        Console.WriteLine(Total9);
+        Console.WriteLine(Total10);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
