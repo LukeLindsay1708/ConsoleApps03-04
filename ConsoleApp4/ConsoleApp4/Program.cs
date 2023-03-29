c8fea7323407cb6e523563d3f4eea61ccd6d05e5
LukeLindsay1708 <115123472+LukeLindsay1708@users.noreply.github.com>
2023-03-30 00:01:04 +0100

    found file code

--- ConsoleApp4/ConsoleApp4/Program.cs
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("---------------------------------------------------------");
+        Console.WriteLine("----------------- Luke's Social Network -----------------");
+        Console.WriteLine("-----------------  Student ID 22206266 ------------------");
+        Console.WriteLine("-------------- Buckinghamshire University --------------");
+        Console.WriteLine("---------------------------------------------------------");
+
+        Console.WriteLine("Hello, please type in your username:");
+        string userName = Console.ReadLine();
+
+        int menuChoice;
+        do
+        {
+            Console.WriteLine("1) To post a message");
+            Console.WriteLine("2) To post a picture");
+            Console.WriteLine("3) Display all posts");
+            Console.WriteLine("4) Exit");
+
+            Console.WriteLine("Please enter what you would like to do: ");
+            menuChoice = Convert.ToInt32(Console.ReadLine());
+
+            switch (menuChoice)
+            {
+                case 1:
+                    Console.WriteLine("What message would you like to post?");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Please paste (CTRL + V) the URL of the picture you would like to post:");
+                    break;
+
+                case 3:
+                    Console.WriteLine("All posts:");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Exit Program");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+
+            Console.WriteLine();
+        } while (menuChoice != 4);
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
